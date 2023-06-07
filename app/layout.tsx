ab95be0c38ec84494398b5aeb011c236a6af9373
Hynek Fišera <hynek.fisera@seznam.cz>
2023-06-07 16:49:17 +0200

    Add GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { Metadata } from "next";
 import Link from "next/link";
+import GAWrapper from "./GAWrapper";
 
 export const metadata: Metadata = {
   title: "Interactive ASMR Experience",
@@ -14,6 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
+        <GAWrapper />
         <header className="absolute top-0 left-0 right-0 z-50 p-4 flex flex-row justify-center sm:justify-start">
           <Link href="/" className="text-xl font-semibold tracking-tighter text-gray-300 select-none">
             interactiveASMR
