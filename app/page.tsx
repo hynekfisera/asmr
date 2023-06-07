60880f01e64204b5959943d0f924958fee2ea127
Hynek Fišera <hynek.fisera@seznam.cz>
2023-06-07 09:30:59 +0200

    Add links

--- app/page.tsx
+++ app/page.tsx
@@ -39,6 +39,22 @@ export default function Home() {
   return (
     <main className="flex flex-col px-6 py-12 md:py-6">
       <h1 className="mt-12 text-2xl md:text-3xl font-light text-gray-200 shadow text-center sm:text-left">Interactive ASMR experience</h1>
+      <div className="mt-1 text-xs text-gray-400 flex flex-wrap gap-1">
+        <p>
+          Created by{" "}
+          <Link href="https://hynekfisera.com/" target="_blank" rel="noopener noreferrer" className="hover:underline">
+            Hynek Fišera
+          </Link>
+        </p>{" "}
+        -
+        <Link href="https://github.com/hynekfisera/asmr" target="_blank" rel="noopener noreferrer" className="hover:underline">
+          Star on GitHub
+        </Link>{" "}
+        -
+        <Link href="https://www.buymeacoffee.com/hynekfisera" target="_blank" rel="noopener noreferrer" className="hover:underline">
+          Buy Me a Coffee
+        </Link>
+      </div>
       <div className="mt-6 grid sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 gap-6">
         {items.map((item) => (
           <Link href={item.disabled ? "#" : item.href} key={item.href} className={`flex flex-col bg-gray-200/5 px-6 py-4 rounded-lg border-2 border-transparent ${item.disabled ? "cursor-not-allowed opacity-50" : "cursor-pointer hover:border-indigo-500"}`}>
